39ece87570cfdb7711bdf66e37b56604ef1c4e1c
Irina Chekushkina <zwoniii@ya.ru>
2025-02-12 09:03:37 +0400

    ASP.NET Core Web API .NET 8 2024 - 5. DTOs

---
+++ api/Controllers/StockControllers.cs
@@ -0,0 +1,38 @@
+﻿using api.Data;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[Route("api/stock")]
+[ApiController]
+public class StockControllers : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    public StockControllers(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks.ToList()
+            .Select(s => s.ToStockDto());
+        
+        return Ok(stocks);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var stock = _context.Stocks.Find(id);
+
+        if (stock == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(stock.ToStockDto());
+    }
+}
